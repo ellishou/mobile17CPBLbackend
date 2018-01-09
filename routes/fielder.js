a2fa092836c666e88dbefeb0bbad00af2058cf7c
Ellis <EllisHou@ami.com.tw>
2018-01-09 20:06:20 +0800

    delete no use code

--- routes/fielder.js
+++ routes/fielder.js
@@ -5,60 +5,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	console.log('req.query',req.query);
-	console.log('req.query.sort',req.query.sort);
-	// var sql = " SELECT "+
-	// 		" f.YearMonth as '年度月份', "+
-	// 		" f.PlayerName as'球員名稱', "+
-	// 		" f.HitL AS '打擊L' , "+
-	// 		" f.HitR as '打擊R' , "+
-	// 		" f.Pow as '力量' , "+
-	// 		" f.Eye as '選球' , "+
-	// 		" f.Agi as '跑壘' , "+
-	// 		" f.Def as '守備' , "+
-	// 		" f.Pass as '傳球' , "+
-	// 		" pl.LevelName as '卡片等級' , "+
-	// 		" t.TeamNameEN as '隊伍' , "+
-	// 		" p.PositionName as '守位' ,"+
-	// 		" fp.Value as '守備適性' "+
-	// 		" FROM "+
-	// 		" team t , "+
-	// 		" fielderteam ft , "+
-	// 		" playerlevel pl , "+
-	// 		" fielder f "+
-	// 		" left join fielderposition fp on fp.FielderID = f.ID "+
-	// 		" left join dposition p on fp.PositionID = p.ID "+
-	// 		" WHERE "+
-	// 		" f.ID = ft.fielderID AND t.ID = ft.teamID "+
-	// 		" AND f.PlayerLevel = pl.ID ";
-	// var sql = " SELECT "+
- //    " f.YearMonth AS '年度月份', "+
- //    " f.PlayerName AS '球員名稱', "+
- //    " f.HitL AS '打擊L', "+
- //    " f.HitR AS '打擊R', "+
- //    " f.Pow AS '力量', "+
- //    " f.Eye AS '選球', "+
- //    " f.Agi AS '跑壘', "+
- //    " f.Def AS '守備', "+
- //    " f.Pass AS '傳球', "+
- //    " FLOOR((f.HitL+f.HitR+f.Pow+f.Eye+f.Agi+f.Def+f.Pass)/7) as '評價', "+
- //    " pl.LevelName AS '卡片等級', "+
- //    " GROUP_CONCAT(DISTINCT t.TeamNameAlias) AS '隊伍', "+
- //    " GROUP_CONCAT(p.PositionName, '-', fp.Value) AS '守備適性' "+
-	// " FROM "+
-	//     " playerlevel pl, "+
-	//     " fielder f "+
-	//     "    LEFT JOIN "+
-	//     " fielderposition fp ON f.ID = fp.FielderID "+
-	//     "    LEFT JOIN "+
-	//     " fielderteam ft ON f.ID = ft.FielderID "+
-	//     "    LEFT JOIN "+
-	//     " dposition p ON fp.PositionID = p.ID "+
-	//     "    LEFT JOIN "+
-	//     " team t ON ft.TeamID = t.ID "+
-	// " WHERE "+	
-	//     "f.PlayerLevel = pl.ID "+
-	// " GROUP BY f.ID , pl.ID; ";
 	var sql = " select "+
 			"  res.ID as id,  "+
 			"  res.YearMonth AS '年度月份',  "+
@@ -111,7 +57,6 @@ router.get('/', function(req, res) {
 		sql += " Order BY "+sort[0]+" "+sort[1];
 	}
 
-	//console.log(sql);
 	myDB.query(sql,function(err, results) {
 	    if(err) 
 		{ 
