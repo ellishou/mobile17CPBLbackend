864cdc2fdf2f614c7ce68ebf226637af5e858cbf
Ellis <EllisHou@ami.com.tw>
2018-01-19 17:33:13 +0800

    add year month sql block , but finish not yet

---
+++ common.js
@@ -0,0 +1,11 @@
+exports.makeIterator = function(array) {
+    var nextIndex = 0;
+    
+    return {
+       next: function() {
+           return nextIndex < array.length ?
+               {value: array[nextIndex++], done: false} :
+               {done: true};
+       }
+    };
+}
