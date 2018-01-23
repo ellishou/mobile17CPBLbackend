4d3cc427852a15d6c1df1fe05b9ce62daf87ca94
EllisHou <ellishou@iii.org.tw>
2018-01-23 23:08:48 +0800

    update online database

--- DBConfig.js
+++ DBConfig.js
@@ -1,9 +1,16 @@
 module.exports = {
 	/* 数据库配置参数 */
-	'db_host': '127.0.0.1',
-	'db_port':  '13306',
-	'db_name': 'mobileCPBL17',
-	'db_user': 'root',
-	'db_passwd': '1qaz@WSX',
+	// 'db_host': '127.0.0.1',
+	// 'db_port':  '13306',
+	// 'db_name': 'mobileCPBL17',
+	// 'db_user': 'root',
+	// 'db_passwd': '1qaz@WSX',
+	// 'db_conn_limit': 10
+
+	'db_host': 'sql12.freemysqlhosting.net',
+	'db_port':  '3306',
+	'db_name': 'sql12215919',
+	'db_user': 'sql12215919',
+	'db_passwd': 'IESAjAxk5t',
 	'db_conn_limit': 10
 };
